58e1a380483ee09abcb052baa3519e10d740f5cb
neo-blyton <chidam.chidam@hotmail.com>
2016-11-19 16:22:10 +0800

    add-email-SES-chnges

--- src/com/tapiver/notification/constants/NotificationConfigurationConstants.java
+++ src/com/tapiver/notification/constants/NotificationConfigurationConstants.java
@@ -8,6 +8,8 @@ package com.tapiver.notification.constants;
  *
  */
 public interface NotificationConfigurationConstants {
+	
+	//AWS SMS Notification
 	static final String AWS_SenderID = "AWS.SNS.SMS.SenderID";
 	static final String AWS_MaxPrice = "AWS.SNS.SMS.MaxPrice";
 	static final String AWS_SMSType = "AWS.SNS.SMS.SMSType";
@@ -16,4 +18,8 @@ public interface NotificationConfigurationConstants {
 	static final String AWS_MaxPrice_Val = "0.50"; //TODO RAJ: To check with Erwin
 	static final String AWS_SMSType_Val = "Transactional";
 	
+	
+	//AWS SES Email Notification
+	static final String EMAIL_FROM_ADDRESS = "NOTIFICATIONS@TAPIVER.COM"; 
+    
 }
--- src/com/tapiver/notification/service/impl/NotificationServiceImpl.java
+++ src/com/tapiver/notification/service/impl/NotificationServiceImpl.java
@@ -6,6 +6,14 @@ package com.tapiver.notification.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+//import com.amazonaws.regions.Region;
+//import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.MessageAttributeValue;
 import com.amazonaws.services.sns.model.PublishRequest;
@@ -15,7 +23,6 @@ import com.tapiver.notification.service.NotificationService;
 import com.tapiver.notification.service.exception.NotificationException;
 import com.tapiver.notification.vo.EmailNotificationParameter;
 import com.tapiver.notification.vo.SmsNotificationParameter;
-
 /**
  * @author raj
  *
@@ -59,8 +66,26 @@ public class NotificationServiceImpl implements NotificationService {
 	 */
 	@Override
 	public boolean sendEmailNotification(EmailNotificationParameter parameter) throws NotificationException {
-		// TODO Auto-generated method stub
-		return false;
+
+		String subject = parameter.getSubject();
+		String message = parameter.getMessage(); //body; Explore HTML option
+        String destinationEmailAddress = parameter.getEmailAddress();//"+65XXXXXXXX";
+		
+        boolean emailSentSuccessfully = false;
+        
+        //Attempt sms delivery with Amazon SNS
+        try{
+        	sendEmailNotificationWithAWS(NotificationConfigurationConstants.EMAIL_FROM_ADDRESS,destinationEmailAddress,subject, message);
+        	emailSentSuccessfully = true;
+        }catch(Exception e){
+        	System.out.println("SMS notification was not successful with AWS. Attempting with Twilio...");
+        }
+        
+        if(!emailSentSuccessfully){
+        	//TODO RAJ: Twilio implementation pending
+        	//Attempt with Twilio
+        }
+		return emailSentSuccessfully;
 	}
 
 	/**
@@ -100,9 +125,50 @@ public class NotificationServiceImpl implements NotificationService {
 	        System.out.println(result.toString()); 
 	}
 	
-	public static void main(String[] args) {
+	private void sendEmailNotificationWithAWS(String fromAddress, String toAddress, String subject, String body){
+		Destination destination = new Destination().withToAddresses(new String[]{toAddress});
+		
+		// Create the subject and body of the message.
+        Content subj = new Content().withData(subject);
+        Content textBody = new Content().withData(body);
+        Body bodyMessage = new Body().withText(textBody);
+        
+        // Create a message with the specified subject and body.
+        Message message = new Message().withSubject(subj).withBody(bodyMessage);
+
+        // Assemble the email.
+        SendEmailRequest request = new SendEmailRequest().withSource(fromAddress).withDestination(destination).withMessage(message);
+
+/*        try {
+            System.out.println("Attempting to send an email through Amazon SES by using the AWS SDK for Java...");
+
+            BasicAWSCredentials credentials = null;
+            try {
+                credentials = new BasicAWSCredentials("access_key_id", "secret_key_id"); // TODO RAJ: Need to input the right credentials
+            } catch (Exception e) {
+                //TODO RAJ: Handle exception
+            }
+
+            // Instantiate an Amazon SES client, which will make the service call with the supplied AWS credentials.
+            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials);
+
+            Region REGION = Region.getRegion(Regions.US_EAST_1); //SG doesnt have email privileges
+            client.setRegion(REGION);
+
+            // Send the email.
+            client.sendEmail(request);
+            System.out.println("Email sent!");
+
+        } catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+        }*/
+        
+	}
+	
+/*	public static void main(String[] args) {
         String message = "Your login PIN for Tapiver application is 123456";
         String phoneNumber = "+6592704773";
         new NotificationServiceImpl().sendSMSMessageWithAWS(message, phoneNumber);
-}
+	}*/
 }
